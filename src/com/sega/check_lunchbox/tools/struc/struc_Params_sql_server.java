2d087028bd281042b91d9b0ab9b0d340a020d00e
dem4ply <dem4ply@gmail.com>
2013-03-12 14:19:15 -0700

    parametros para formar la cadena de conecion de sql server

---
+++ src/com/sega/check_lunchbox/tools/struc/struc_Params_sql_server.java
@@ -0,0 +1,12 @@
+package com.sega.check_lunchbox.tools.struc;
+
+public class struc_Params_sql_server
+{
+	public String url = "jdbc:jtds:sqlserver://";
+	public String server = "72.167.245.123";
+	public String port = "1500";
+	public String database = "BDUniversiad";
+	public String user = "UNIVERSIADA2013";
+	public String pass = "UAS2013";
+	public String select_method = "Direct";
+}
